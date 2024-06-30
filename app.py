268fb7857c2160a958fccdf20d00875efe6c7f18
Joraj J R <myselfjoraj@gmail.com>
2024-06-30 08:06:32 +0530

    Initial commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from auth.login_system import LoginSystem
 
 app = Flask(__name__)
 
-cred = firebase_admin.credentials.Certificate('service.json')
+cred = firebase_admin.credentials.Certificate('cloud_key.json')
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': "https://exalearn-77758-default-rtdb.asia-southeast1.firebasedatabase.app"
